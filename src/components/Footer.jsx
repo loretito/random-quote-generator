6337c160839bf802cf5aa199b07af7d0a6fc543f
loreto <loreto.nancucheo@gmail.com>
2023-07-12 22:10:59 -0400

    fix margin bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="flex justify-center mt-16 font-mono text-sm text-[#BDBDBD] fixed bottom-0 bg-white pb-2 pt-2 w-screen">
+    <div className="flex justify-center font-mono text-sm text-[#BDBDBD] fixed bottom-0 bg-white pb-2 pt-2 w-screen">
       <div
         className="flex hover:cursor-pointer"
         onClick={() => window.open("https://github.com/loretito", "_black")}
